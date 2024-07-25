6b8771d5f6c3d1be91648a905818df6a8f992ffa
Gabriela <20200543@ricaldone.edu.sv>
2024-07-24 19:43:08 -0600

    pacientes

--- app/src/main/java/gabriela/marcos/bloom/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/gabriela/marcos/bloom/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package gabriela.marcos.bloom.ui.dashboard
 
+import RecicleViewHelpers.Adaptador
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gabriela.marcos.bloom.R
 import gabriela.marcos.bloom.databinding.FragmentDashboardBinding
+import gabriela.marcos.bloom.login
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.Paciente
+import modelo.claseConexion
 
 class DashboardFragment : Fragment() {
 
@@ -28,9 +39,56 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val rcvPacientes2 = root.findViewById<RecyclerView>(R.id.rcvPacientes2)
+        rcvPacientes2.layoutManager = LinearLayoutManager(requireContext())
+
+        fun obtenerDatosPacientes(): List<Paciente> {
+            val objConexion = claseConexion().cadenaConexion()
+            val idEnfermera = login.variablesGlobalesRecuperacionDeContrasena.IdEnfermera
+
+            // Verifica el nombre de la columna
+            val statement = objConexion?.prepareStatement("SELECT * FROM paciente ")
+            statement?.setString(1, idEnfermera)
+            val resultSet = statement?.executeQuery()!!
+
+            val listaPacientes = mutableListOf<Paciente>()
+
+            while (resultSet.next()) {
+                val idPaciente = resultSet.getString("idPaciente")
+                val nombresPaciente = resultSet.getString("nombresPaciente")
+                val apellidosPaciente = resultSet.getString("apellidosPaciente")
+                val edad = resultSet.getInt("edad")
+                val enfermedad = resultSet.getString("enfermedad")
+                val numeroHabitacion = resultSet.getInt("numeroHabitacion")
+                val numeroCama = resultSet.getInt("numeroCama")
+                val medicamentosAsignados = resultSet.getString("medicamentosAsignados")
+                val fechaIngreso = resultSet.getString("fechaIngreso")
+                val horaDeAplicacionDeMedicamentos = resultSet.getString("horaDeAplicacionDeMedicamentos")
+
+                val paciente = Paciente(
+                    idPaciente,
+                    idEnfermera,
+                    nombresPaciente,
+                    apellidosPaciente,
+                    edad,
+                    enfermedad,
+                    numeroHabitacion,
+                    numeroCama,
+                    medicamentosAsignados,
+                    fechaIngreso,
+                    horaDeAplicacionDeMedicamentos
+                )
+                listaPacientes.add(paciente)
+            }
+            return listaPacientes
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val pacienteDB = obtenerDatosPacientes()
+            withContext(Dispatchers.Main) {
+                val adapter = Adaptador(pacienteDB)
+                rcvPacientes2.adapter = adapter
+            }
         }
         return root
     }
